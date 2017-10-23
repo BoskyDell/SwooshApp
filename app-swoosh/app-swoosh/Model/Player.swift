4c77012b92668f1d5a1c9a7ae80eaa6f4eef8b04
Kevin Keefe <kevin@boskydell.tech>
2017-10-23 17:04:24 -0400

    added league struct

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Kevin Keefe on 10/23/17.
+//  Copyright © 2017 Kevin Keefe. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
